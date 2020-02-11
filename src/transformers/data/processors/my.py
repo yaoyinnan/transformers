b9f7e2ebdd98205fb755b52c9f69b7e1343322cf
A <875995682@qq.com>
2020-02-11 10:23:58 +0800

    上传多模态运行脚本

--- src/transformers/data/processors/my.py
+++ src/transformers/data/processors/my.py
@@ -711,9 +711,9 @@ class Fakeddit2wayProcessor(DataProcessor):
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train_496094.tsv")))
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.tsv")))
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "train_496094.tsv"), '"'), "train")
+            self._read_tsv(os.path.join(data_dir, "train.tsv"), '"'), "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
